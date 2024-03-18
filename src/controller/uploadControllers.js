11aa552485cc232894704c80b5de045bc3665fb7
Gautam-121 <gautamdhakate1234@gmail.com>
2024-03-18 15:23:11 +0530

    update the code upto videoJson Data storage

--- src/controller/uploadControllers.js
+++ src/controller/uploadControllers.js
@@ -4,14 +4,7 @@ const VideoModel = require("../model/videoSchema");
 const createVideoData = async (req, res) => {
   try {
 
-    console.log("Enter")
     const data = req.body
-    const file = req.files
-
-    console.log(file)
-
-    console.log(data)
-    console.log(Object.keys(data).length == 0)
 
     if(Object.keys(data).length == 0){
       return res.status(400).json({
@@ -20,15 +13,11 @@ const createVideoData = async (req, res) => {
       })
     }
 
-    console.log(data)
-
     const videoData = await VideoModel.create({
       video_id: data.id,
       title: data.title,
       videoSelectedFile:data.selectedVideo
     });
-
-    console.log("Exist" ,videoData)
 
     return res.status(201).json({
       success: true,
@@ -142,86 +131,6 @@ const getVideoById = async (req, res) => {
   }
 }
 
-// const updateVideoById = async (req, res, next) => {
-//   try {
-
-//     if (!req.params.id) {
-//       return res.status(400).json({
-//         success: false,
-//         message: "video_id is Missing",
-//       });
-//     }
-
-//     const isVideoDataExist = await VideoModel.findByPk(req.params.id);
-
-//     if (!isVideoDataExist) {
-//       return res.status(404).json({
-//         success: false,
-//         message: "videoData Not Found",
-//       });
-//     }
-
-//     let datas = {};
-//     const data = JSON.parse(JSON.stringify(req.body));
-
-//     if (req?.files?.["video"]) {
-
-//       const filePathToDelete = path.join(
-//         "uploads/", isVideoDataExist.video_path
-//       );
-
-//       try {
-//         await fs.promises.unlink(filePathToDelete);
-//         console.log(`File deleted: ${filePathToDelete}`);
-
-//         const videoFilePath = req?.files["video"]?.[0]?.filename;
-//         datas["video_path"] = videoFilePath;
-
-//       } catch (err) {
-//         console.log(`Error deleting file: ${filePathToDelete}`);
-//       }
-//     }
-
-//     if (req?.files?.["vtt"]) {
-
-//       if (isVideoDataExist.vtt_path) {
-//         const filePathToDelete = path.join(
-//           "uploads/", isVideoDataExist.vtt_path
-//         );
-
-//         try {
-//           await fs.promises.unlink(filePathToDelete);
-//           console.log(`File deleted: ${filePathToDelete}`);
-
-//           const vttFilePath = req.files["vtt"][0].filename;
-//           datas["vtt_path"] = vttFilePath;
-
-//         } catch (err) {
-//           console.error(`Error deleting file: ${filePathToDelete}`);
-//         }
-//       }
-//     }
-
-//     // Update the project based on the ID
-//     const [, updatedProject] = await VideoModel.update({ ...data, ...datas }, {
-//       where: {
-//         video_id: req.params.id,
-//       },
-//       returning: true, // This ensures that the updated row is returned
-//     });
-
-//     return res.status(201).json({
-//       success: true,
-//       message: "Video Data Created Successfully",
-//       updatedProject
-//     });
-//   } catch (error) {
-//     res.status(500).send({
-//       success: false,
-//       message: error.message,
-//     });
-//   }
-// };
 
 
 module.exports = { uploadVideo, createVideoData , getAllVideo, getVideoById }
--- src/routes/uploadRoutes.js
+++ src/routes/uploadRoutes.js
@@ -1,25 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const { getAllVideo, getVideoById  } = require("../controller/uploadControllers.js");
-// const multer = require("multer");
-// const path = require("path")
-
-// const storage = multer.diskStorage({
-//     destination: "./uploads",
-//     filename: function (req, file, cb) {
-//         cb(null, file.fieldname + "-" + Date.now() + path.extname(file.originalname));
-//     },
-// });
-
-// const upload = multer({ storage: storage });
-
-// router.post("/upload/videoData", uploadMediaData);
 
 router.get("/getAllVideo", getAllVideo);
 
 router.get("/getVideoById/:id", getVideoById)
-
-// router.post("/upload/videos", upload.fields([{ name: "video" }]), uploadVideos);
-
 
 module.exports = router;
