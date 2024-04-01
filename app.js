ad401541ede753cfe3973c8f32ab0830cd5cdf2c
Gautam-121 <gautamdhakate1234@gmail.com>
2024-04-01 14:34:15 +0530

    update package file

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 require("dotenv").config();
 const express = require("express");
 const errorMiddleware = require("./middlewares/error.middleware.js");
-const { createProxyMiddleware } = require('http-proxy-middleware');
 const app = express();
 const cors = require("cors");
 
@@ -16,15 +15,7 @@ const userRouter = require("./routes/user.routes.js")
 const uploadRouter = require("./routes/video.routes.js");
 const quetionRouter = require("./routes/questions.routes.js");
 
-// Define your proxy middleware
-const apiProxy = createProxyMiddleware('/api', {
-    target: 'http://139.5.190.56:8081', // Your backend API endpoint
-    changeOrigin: true, // Required for virtual hosted sites
-    secure: false, // Allow proxying to HTTPS targets with invalid SSL certificates
-    // Additional options as needed
-  });
   
-
 app.get("/",(req,res,next)=>{
     return res.status(200).json({
         success: true,
