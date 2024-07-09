f9bd95f6cf4a96d7b011537ea965db01d8ce5b8e
Gautam-121 <gautamdhakate1234@gmail.com>
2024-07-09 15:52:37 +0530

    add plan hasMany relation

--- models/planrestrict.model.js
+++ models/planrestrict.model.js
@@ -12,8 +12,19 @@ const PlanRestrict = sequelize.define("planRestrict" , {
 })
 
 // Make sure Video is defined before creating the association
-PlanRestrict.belongsTo(Video, { foreignKey: "videoId", as: "video" });
-Video.hasMany(PlanRestrict, { as: 'plans' });
+// PlanRestrict.belongsTo(Video, { foreignKey: "videoId", as: "video" });
+// Video.hasMany(PlanRestrict, { as: 'plans' });
+
+// Define the associations
+Video.hasMany(PlanRestrict, { 
+    foreignKey: 'videoId',
+    as: 'plans'  // This is how you'll access PlanRestricts from a Video instance
+  });
+  
+  PlanRestrict.belongsTo(Video, { 
+    foreignKey: 'videoId',
+    as: 'video'  // This is how you'll access the Video from a PlanRestrict instance
+  });
 
 module.exports = PlanRestrict
 
